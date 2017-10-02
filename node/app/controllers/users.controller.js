d5efd9911944b33cde7ffb33b56007e95f2362e6
julian <julian-sa@hotmail.es>
2017-10-02 00:23:58 -0500

    Feat was added CRUD operations to User requirement

--- node/app/controllers/users.controller.js
+++ node/app/controllers/users.controller.js
@@ -7,26 +7,54 @@ const User = require('../models/user.model');
  * @property {string} req.body.password
 */
 
-
-
 function create(req, res, next){
     const user = new User({
         user_name: req.body.user_name,
         name: req.body.name,
         password: req.body.password
     });
-    console.log(user);
     user.save()
         .then(saveUser => res.json(saveUser))
         .catch(e => next(e));
 
 }
 
-// function get(req, res,netx){
-//     User.find({},)
-// }
+function get(req, res, next){
+    const projection = ["user_name", "name"]
+    User.find({},projection,function(err, users){
+        if (err) return err;
+        res.json(users)
+    })
+}
 
+function getById(req, res, next){
+    const userId = req.params.userid
+    const query = {"_id": userId}
+    User.findOne(query, function(err, user){
+        if (err) res.send(`User with ${userId} not found`);
+        res.send(user);
+    })
+}
 
+function deleteUser(req, res, next){
+    User.deleteOne(req.body, function(err, user){
+        if (err) res.send(`There were something bad, try again`)
+        res.send(`user with object id : ${req.body._id} was delete succesfull`)
+    })
+}
+
+function updateUser(req, res, next){
+    const queryUpdate = req.body
+    const queryMatch = {'_id': req.body._id}
+    User.where(queryMatch).update({$set: queryUpdate}, function(err, updateUser){
+        if (err) res.send('There was a problem')
+        res.send('User information update successful')
+    })
+}
 
 
 module.exports.create = create;
+module.exports.getById = getById;
+module.exports.get = get;
+module.exports.deleteUser = deleteUser;
+module.exports.updateUser = updateUser;
--- node/app/routes/user.route.js
+++ node/app/routes/user.route.js
@@ -5,10 +5,24 @@ const userCtrl = require('../controllers/users.controller');
 const router =  express.Router();
 
 router.route('/users')
- /** /api/users create new user*/
+ /** /api/users create new user */
     .post(userCtrl.create)
 
-    // .get(userCtrl.get);
+    .get(userCtrl.get)
+
+
+router.route('/user')
+/* Delete and updateuser passing user id*/
+    .delete(userCtrl.deleteUser)
+    .put(userCtrl.updateUser)
+
+
+router.route('/user/:userid')
+//** get users acording to id*/
+    .get(userCtrl.getById)
+    
+
 
     
 module.exports = router;
+
