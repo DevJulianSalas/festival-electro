9a1e95f671392829d5a26b5784ab26168225746c
julian <juliansalas080@gmail.com>
2018-06-15 19:00:26 -0500

    its changed navbar and header

--- frontend/app/festival_electro/src/components/App/App.js
+++ frontend/app/festival_electro/src/components/App/App.js
@@ -3,18 +3,27 @@ import './App.css';
 import Header from '../Header/Header'
 import Main from '../Main/Main'
 import Footer from '../Footer/Footer'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
-
+//base styles
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: "#fafafa" }, 
+    secondary: { main: '#11cb5f'}, 
+  },
+});
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header/>
-        <Main/>
-        <Footer/>
-      </div>
+      <MuiThemeProvider theme={theme} style={{ textDecoration: 'none' }}>
+        <div className="App" style={{ textDecoration: 'none' }}>
+          <Header/>
+          <Main/>
+          <Footer/>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
--- frontend/app/festival_electro/src/components/Header/Header.js
+++ frontend/app/festival_electro/src/components/Header/Header.js
@@ -3,12 +3,21 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { withStyles } from '@material-ui/core/styles';
+import Icon from '@material-ui/core/Icon';
+import { Grid } from '@material-ui/core';
 
 
 const styles = {
-  colorPrimary: '#fafafa'
+  root:{
+    flexGrow: 1,
+  },
+  flex:{
+    flex: 1
+  },
+  colorPrimary: '#fafafa',
+
 }
 
 
@@ -23,19 +32,22 @@ class Header extends React.Component{
   render() {
     const { classes } = this.props
     return(
-      <div>
-        <AppBar position="static" color={classes.colorPrimary}>
-          <Toolbar>
-            <Typography variant="title" color="inherit">
-              <NavLink to="/">Home</NavLink>
-            </Typography>
-            <Button color="inherit"><NavLink to="/evento">crea un evento</NavLink></Button>
-            <Button color="inherit"><NavLink to="/registrarse">Registrarse</NavLink></Button>
-            <Button color="inherit"><NavLink to="/Ingresar">Ingresar</NavLink></Button>
+      <div className={classes.root}>
+        <AppBar position="static" color="primary" >
+          <Toolbar alignItems="center" direction="column">
+            <div className={classes.flex}>
+              <Link to="/"><img src="" alt="Logo"/></Link>
+            </div>
+            <nav>
+              <ul>
+                <Link to="/notificaciones"><i class="far fa-bell"></i>Notificaciones</Link>
+                <Link to="/iniciar-sesion"><i class="fas fa-sign-in-alt"></i>Iniciar sesion</Link>
+                <Link to="/registrarse"><i class="fas fa-user-plus"></i>Registrarse</Link>
+              </ul>
+            </nav>
           </Toolbar>
         </AppBar>
       </div>
-      
     );
   }
 }
